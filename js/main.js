64944ee7d8324db292f064bb1991ff999865d6d3
oldmc <oldmcdonnell@gmail.com>
2024-03-29 11:40:00 -0400

    fixing alarm

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function startTime() {
     var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
     //this variable contains the time as a string
-    var string = h + ":" + m + ":" + s;
+    var string = hours + ":" + minutes + ":" + seconds;
     //this converts string to a set of HTML img tags containing images
     var img = stringToImage(string);
     //this puts the time on the website
@@ -14,6 +14,14 @@ function startTime() {
       string + "<br>" + img;
     //this loops the function every 1000 milliseconds (1 second)
     var t = setTimeout(startTime, 1000);
+    if (hours < 0) {
+        hours = hours * -1;
+    } else if (hours == 00) {
+        hours = 12;
+    } else {
+        hours = hours;
+    }
+    
   }
   
   function checkTime(i) {
@@ -52,4 +60,148 @@ function startTime() {
     "0": "img/0.jpg",
     ":": "img/colon.jpg",
   }
+
+
+  
+function addZero(time) {
+
+    return (time < 10) ? "0" + time : time;
+
+}
+
+function hoursMenu(){
+
+var select = document.getElementById('alarmhrs');
+var hrs = 12
+
+for (i=1; i <= hrs; i++) {
+    select.options[select.options.length] = new Option( i < 10 ? "0" + i : i, i);
+    
+}
+}
+hoursMenu();
+
+function minMenu(){
+
+var select = document.getElementById('alarmmins');
+var min = 59;
+
+for (i=0; i <= min; i++) {
+    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+}
+}
+minMenu();
+
+function secMenu(){
+
+var select = document.getElementById('alarmsecs');
+var sec = 59;
+
+for (i=0; i <= sec; i++) {
+    select.options[select.options.length] = new Option(i < 10 ? "0" + i : i, i);
+}
+}
+secMenu();
+
+
+function alarmSet() {
+
+var hr = document.getElementById('alarmhrs');
+
+var min = document.getElementById('alarmmins');
+
+var sec = document.getElementById('alarmsecs');
+
+var ap = document.getElementById('ampm');
+
+
+var selectedHour = hr.options[hr.selectedIndex].value;
+var selectedMin = min.options[min.selectedIndex].value;
+var selectedSec = sec.options[sec.selectedIndex].value;
+var selectedAP = ap.options[ap.selectedIndex].value;
+
+var alarmTime = addZero(selectedHour) + ":" + addZero(selectedMin) + ":" + addZero(selectedSec) + selectedAP;
+console.log('alarmTime:' + alarmTime);
+
+document.getElementById('alarmhrs').disabled = true;
+document.getElementById('alarmmins').disabled = true;
+document.getElementById('alarmsecs').disabled = true;
+document.getElementById('ampm').disabled = true;
+
+
+//when alarmtime is equal to currenttime then play a sound
+var h2 = document.getElementById('clock');
+
+/*function to calcutate the current time 
+then compare it to the alarmtime and play a sound when they are equal
+*/
+
+setInterval(function(){
+
+var date = new Date();
+
+var hours = (12 - (date.getHours()));
+// var hours = date.getHours();
+
+var minutes = date.getMinutes();
+
+var seconds = date.getSeconds();
+
+var ampm = (date.getHours()) < 12 ? 'AM' : 'PM';
+
+
+//convert military time to standard time
+
+if (hours < 0) {
+    hours = hours * -1;
+} else if (hours == 00) {
+    hours = 12;
+} else {
+    hours = hours;
+}
+
+var currentTime = h2.textContent = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds) + "" + ampm;
+
+
+if (alarmTime == currentTime) {
+    sound.play();
+    alert(`Alarm! Alarm!`);
+    }
+
+},1000);
+
+
+// console.log('currentTime:' + currentTime);	
+
+}
+
+
+function alarmClear() {
+
+document.getElementById('alarmhrs').disabled = false;
+document.getElementById('alarmmins').disabled = false;
+document.getElementById('alarmsecs').disabled = false;
+document.getElementById('ampm').disabled = false;
+sound.pause();
+}
+
+
+
+  
+
+  
+
+
+  
+  
+  function alarmClear() {
+  
+      document.getElementById('alarmhrs').disabled = false;
+      document.getElementById('alarmmins').disabled = false;
+      document.getElementById('alarmsecs').disabled = false;
+      document.getElementById('ampm').disabled = false;
+      sound.pause();
+  }
+  
+  
   
